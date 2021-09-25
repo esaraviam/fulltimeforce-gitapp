74cb08ab3070710885cf934b9792e7ca89bfc6af
Esteban Saravia <estebansaravia@gmail.com>
2021-09-25 17:14:33 -0300

    Adding Tailwindcss as a css framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto px-4 py-0 bg-green-500">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
